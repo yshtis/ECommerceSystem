0f521e45c41668026b2b854f9e60046294a85166
yshtis <yshtis404@gmail.com>
2025-09-22 18:05:26 +0900

    Initial commit

---
+++ src/main/java/com/unknownshop/ecommerce/ECommerceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.unknownshop.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSystemApplication.class, args);
+	}
+
+}
